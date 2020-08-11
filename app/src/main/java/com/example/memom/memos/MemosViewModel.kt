f7311c8b4d531ee334f295780ca659b6ec46f4d2
penuwo <re.o.spw21@gmail.com>
2020-08-11 12:27:02 +0900

    表示用のTextをViewModelに用意

--- app/src/main/java/com/example/memom/memos/MemosViewModel.kt
+++ app/src/main/java/com/example/memom/memos/MemosViewModel.kt
@@ -2,5 +2,16 @@ package com.example.memom.memos
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.delay
+import kotlin.random.Random
 
-class MemosViewModel @ViewModelInject constructor() : ViewModel()
+class MemosViewModel @ViewModelInject constructor() : ViewModel() {
+
+    val text = liveData {
+        while (true) {
+            emit(Random.nextInt().toString())
+            delay(500L)
+        }
+    }
+}
