17b6b6aa2bdfe59049a0cda8caa0d9a29a69d9ae
penuwo <re.o.spw21@gmail.com>
2020-08-11 12:30:28 +0900

    委譲プロパティを用いてBindingにViewModelを渡す

--- app/src/main/java/com/example/memom/memos/MemosFragment.kt
+++ app/src/main/java/com/example/memom/memos/MemosFragment.kt
@@ -1,12 +1,23 @@
 package com.example.memom.memos
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.memom.R
+import com.example.memom.common.Binding
+import com.example.memom.databinding.FragmentMemosBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MemosFragment : Fragment(R.layout.fragment_memos) {
 
     private val viewModel: MemosViewModel by viewModels()
+    private val binding: FragmentMemosBinding by Binding()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.viewModel = viewModel
+    }
 }
