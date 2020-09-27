e139950069e5fd7f069c466a52cc8bc828cd1104
penuwo <re.o.spw21@gmail.com>
2020-09-28 02:03:21 +0900

    メモ追加のBottomSheetDialogFragmentを作成

--- app/src/main/java/com/example/memom/data/entity/MemoItem.kt
+++ app/src/main/java/com/example/memom/data/entity/MemoItem.kt
@@ -1,5 +1,9 @@
 package com.example.memom.data.entity
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MemoItem(
     val text: String
-)
+) : Parcelable
