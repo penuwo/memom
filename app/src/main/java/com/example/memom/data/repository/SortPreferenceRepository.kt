c4f15c2669e143e489f1a02b1ccc1daae7866d98
penuwo <re.o.spw21@gmail.com>
2020-09-30 03:43:07 +0900

    ソートの設定をSharedPreferencesで保持するRepositoryを作成

---
+++ app/src/main/java/com/example/memom/data/repository/SortPreferenceRepository.kt
@@ -0,0 +1,34 @@
+package com.example.memom.data.repository
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.distinctUntilChanged
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SortPreferenceRepository @Inject constructor(private val sharedPreferences: SharedPreferences) {
+
+    private val _sortPreferenceLiveData = MutableLiveData<SortType>(getSortPreference())
+    val sortPreferenceLiveData: LiveData<SortType> = _sortPreferenceLiveData.distinctUntilChanged()
+
+    fun getSortPreference(): SortType = SortType.values()[sharedPreferences.getInt(SORT_SETTING_KEY, 0)]
+
+    fun setSortPreference(sortType: SortType) {
+        sharedPreferences.edit {
+            putInt(SORT_SETTING_KEY, sortType.ordinal)
+        }
+        _sortPreferenceLiveData.value = sortType
+    }
+
+    companion object {
+        private const val SORT_SETTING_KEY = "sort_setting_key"
+    }
+}
+
+enum class SortType {
+    DateAscending,
+    DateDescending
+}
